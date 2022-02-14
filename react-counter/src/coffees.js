35a904bcf424717ea8a146db33678822a63d3daf
“Keith <KeithRichardinTech@gmail.com>
2022-02-14 11:46:56 -0500

    the end of the the first leason

--- react-counter/src/coffees.js
+++ react-counter/src/coffees.js
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from "react";
 
 function Coffees() {
   const [coffeeList, setCoffeeList] = useState();
+  const [temp, setTemp] = useState("hot");
   useEffect(() => {
-    fetch("https://api.sampleapis.com/coffee/hot")
+    fetch("https://api.sampleapis.com/coffee/${temp}")
       .then((Response) => Response.json())
       .then((data) => setCoffeeList(data))
       .catch((err) => console.error(err));
-  }, []);
+  }, [temp]);
   return (
     <section className="coffee-container" id="coffees">
       <h2>Coffee Recipes</h2>
+      <button onClick={() => setTemp("hot")}>Hot</button>
+      <button onClick={() => setTemp("iced")}>Iced</button>
       {!coffeeList ? (
         <h3>Loading...</h3>
       ) : (
